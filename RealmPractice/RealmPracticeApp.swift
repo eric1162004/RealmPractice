2ee7aaef7a21e7e0ee4097da7e9bd8269d668e82
Eric Cheung <eric1162004@hotmail.com>
2022-03-11 09:21:16 -0800

    Initial Commit

---
+++ RealmPractice/RealmPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmPracticeApp.swift
+//  RealmPractice
+//
+//  Created by eric on 2022-03-11.
+//
+
+import SwiftUI
+
+@main
+struct RealmPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
